d68da36a8f056e05036962d909371318db8c018d
Karolina Drabent <karolina.drabent@tcl.com>
2020-06-14 18:37:00 +0200

    took player arguement to the ConnectN init

--- connect_n/ConnectN.py
+++ connect_n/ConnectN.py
@@ -8,17 +8,16 @@ from connect_n.Player import Player
 class ConnectN(arcade.Window):
     """ Main application class. """
 
-    def __init__(self, width, height, title):
+    def __init__(self, width, height, title, player_1=None, player_2=None):
         super().__init__(width, height, title)
-        self.board = Board(8, 8, 4, None, Player())
+        self.board = Board(8, 8, 4, player_1, player_2)
         arcade.set_background_color(arcade.color.BLUE_BELL)
 
         self.image_paths = [os.path.join("connect_n", "graphics", "field.jpg"),
                             os.path.join("connect_n", "graphics", "elisabeth2-coingreen.jpg"),
-                            os.path.join("connect_n", "graphics", "elisabeth2-coin-pink.jpg"),
-                            os.path.join("connect_n", "graphics", "arrow.jpg")]
+                            os.path.join("connect_n", "graphics", "elisabeth2-coin-pink.jpg")]
 
-        field = arcade.Sprite(os.path.join("connect_n", "graphics", "elisabeth2-coin-pink.jpg"), 1)
+        field = arcade.Sprite(self.image_paths[0], 1)
 
         self.field_width = float(self.width / max(self.board.n_cols, self.board.n_rows))
         self.field_height = float((self.height - 20) / max(self.board.n_cols, self.board.n_rows))
@@ -60,10 +59,12 @@ class ConnectN(arcade.Window):
         """ All the logic to move, and the game logic goes here. """
         # help coin
         self.help_coin.center_x = self.field_width * (self.chosen_col_id + 0.5)
-        self.help_coin.center_y = self.board.find_row(self.chosen_col_id) * self.field_height + self.field_height / 2 if self.board.find_row(self.chosen_col_id) < self.board.n_rows else -10000
+        self.help_coin.center_y = self.board.find_row(
+            self.chosen_col_id) * self.field_height + self.field_height / 2 if self.board.find_row(
+            self.chosen_col_id) < self.board.n_rows else -10000
 
-        if   self.game_over :
-            self.help_coin.alpha=0
+        if self.game_over:
+            self.help_coin.alpha = 0
 
         if not self.game_over:
             if self.board.active_player() is not None:
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from connect_n.ConnectN import ConnectN
+from connect_n.Player import Player
 
 SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 720
@@ -6,6 +7,6 @@ SCREEN_HEIGHT = 720
 SCREEN_TITLE = "Connect4"
 
 if __name__ == '__main__':
-    game = ConnectN(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game = ConnectN(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, player_1=None, player_2=Player())
     game.setup()
     game.run()
